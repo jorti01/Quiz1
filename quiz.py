4f86a9445eb20053c69dfbcc04baaaf83f1fc4df
Juan Ortiz <56034091+jorti01@users.noreply.github.com>
2021-02-28 13:07:20 -0500

    Commit #1

---
+++ quiz.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+
+    @abstractmethod
+    def execute(self) -> None:
+        pass
+
+class EncenderComando(Command):
+	def __init__(self):
+		pass
+	def execute(self):
+		print("Comando de encendido recibido.")
+
+class ApagarComando(Command):
+	def __init__(self):
+		pass
+	def execute(self):
+		print("Comando de apagado recibido.")
+
+class HablarComando(Command):
+	def __init__(self,message):
+		self.message = messsage
+	def execute(self) -> None:
+		print("El robot dice {0}".format(message))
+
+class DormirComando(Command):
+
+	def __init__(self):
+		pass
+
+	def execute(self):
+		print("Comando de dormir recibido.")
+
+class Bot:
+	def __init__(self,command):
+		self.command = command
+
+	def command(self, command):
+		self.command = command
+
+	def ejecutar_comando(self):
+		self.command.execute()
